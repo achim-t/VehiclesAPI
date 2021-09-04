6e6017896c0a6f3321d6e8ca25edcfd115e24ed1
achim-t <achim.t@gmail.com>
2021-09-04 15:07:54 +0200

    Tests

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingTest.java
@@ -0,0 +1,25 @@
+package com.udacity.pricing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PricingTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void findPrice() throws Exception {
+        mvc.perform(get("/prices/1").accept(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isOk());
+    }
+}
