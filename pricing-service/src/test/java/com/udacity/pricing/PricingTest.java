1a3c392a4dcc81bb77c9229045601122044d6d44
achim-t <achim.t@gmail.com>
2021-09-04 15:11:56 +0200

    format

--- pricing-service/src/test/java/com/udacity/pricing/PricingTest.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
